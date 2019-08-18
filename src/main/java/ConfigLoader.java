cd1765b967057aff923f0b34585f15e28319c948
Obstmoehre <jakob.gerstmann@outlook.de>
2019-08-18 22:47:31 +0200

    new and edited comments

--- src/main/java/ConfigLoader.java
+++ src/main/java/ConfigLoader.java
@@ -4,22 +4,23 @@ import com.google.gson.GsonBuilder;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-public class ConfigLoader {
+class ConfigLoader {
     private Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private File config = new File(System.getProperty("user.home") + "/Songreporter/config.json");
 
-    //Configmanager to save config and access the values
-    ConfigManager configManager;
+    // Configmanager to save config and access the values
+    private ConfigManager configManager;
 
-    public ConfigManager load() {
-        //checking and refreshing config file
+    // function to check if the values in the config file are correct
+    ConfigManager load() {
+        // checking if config file exists and creating a new one if not
         try {
             loadConfig();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //getting or if needed setting the path to the browserdriver
+        // getting or if needed setting the path to the browserdriver
         String driverPath = configManager.getDriverPath();
         String dropboxPath = configManager.getDropboxPath();
         File testDriverFile;
@@ -38,6 +39,7 @@ public class ConfigLoader {
             configManager.saveConfig(gson, config);
         }
 
+        // getting or if needed setting the path to the users dropbox directory
         if (dropboxPath.isEmpty()) {
             dropboxPath = new DropboxSelector().selectDropbox();
             configManager.setDropboxPath(dropboxPath);
@@ -55,8 +57,9 @@ public class ConfigLoader {
         return configManager;
     }
 
+    // function to read the config file and create a new configmanager out of this
     private void loadConfig() throws IOException {
-        //testing if the config.json File exists and creating a new one if not
+        // testing if the config.json File exists and creating a new one if not
         boolean isCreated = true;
         if (!(config.exists())) {
             isCreated = new File(config.getParent()).mkdirs();
@@ -67,8 +70,10 @@ public class ConfigLoader {
             }
 
             newConfig();
+        // testing if the config is empty and creating a new one if it is empty
         } else if (!(new BufferedReader(new InputStreamReader(new FileInputStream(config), StandardCharsets.UTF_8)).ready())) {
             newConfig();
+        // reading the file if everything is ok with it
         } else {
             StringBuilder fromJsonBuilder = new StringBuilder();
             BufferedReader bufferedReader;
@@ -85,21 +90,26 @@ public class ConfigLoader {
             configManager = gson.fromJson(fromJsonBuilder.toString().trim(), ConfigManager.class);
         }
 
+        // showing an error window if something went wrong with the config file
         if (!isCreated) {
             ErrorGUI jsonErrorGUI = new ErrorGUI();
             jsonErrorGUI.showNewErrorMessage("Es ist ein Fehler mit der json Datei aufgetreten!");
         }
     }
 
+    // function to create a new and complete config file
     private void newConfig() {
+        // new configmanager to set and save the values
         configManager = new ConfigManager();
 
+        // setting the values
         String driverPath = new DriverSelector().selectDriver();
         configManager.setDriverPath(driverPath);
 
         String dropboxPath = new DropboxSelector().selectDropbox();
         configManager.setDropboxPath(dropboxPath);
 
+        // saving the configuration
         configManager.saveConfig(gson, config);
     }
 }
