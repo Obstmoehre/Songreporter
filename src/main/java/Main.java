0f1eebf56fa2d232620a218a870c7644c0bb3835
Obstmoehre <jakob.gerstmann@outlook.de>
2019-08-18 22:09:48 +0200

    some beauty changes and new comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ConfigLoader configLoader = new ConfigLoader();
-        ConfigManager configManager = configLoader.load();
+        //loading config File
+        ConfigManager configManager = new ConfigLoader().load();
 
+        //reading the ccli songnumbers out of the script
         ArrayList<String> ccliList = new CCLIReader().start(configManager);
 
+        //open browser and report the given ccli songnumbers
         new Reporter().report(configManager, ccliList);
     }
 }
