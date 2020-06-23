705999990e727b4c7793a800478933e5147d36cf
Obstmoehre <jakob.gerstmann@outlook.de>
2020-06-23 19:16:21 +0200

    cleaner code, no password saving, removed comments

--- src/main/java/me/jakob/songreporter/config/ConfigLoader.java
+++ src/main/java/me/jakob/songreporter/config/ConfigLoader.java
@@ -1,10 +1,9 @@
 package me.jakob.songreporter.config;
 
-import me.jakob.songreporter.GUI.DriverSelector;
-import me.jakob.songreporter.GUI.DropboxSelector;
-import me.jakob.songreporter.GUI.ErrorGUI;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import me.jakob.songreporter.GUI.ErrorGUI;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -13,43 +12,21 @@ public class ConfigLoader {
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private final File config = new File(System.getProperty("user.home") + "/Songreporter/config.json");
 
-    // Configmanager to save me.jakob.songreporter.config and access the values
     private ConfigManager configManager;
 
-
-
-    // function to check if the values in the me.jakob.songreporter.config file are correct
     public ConfigManager load() {
-        // checking if me.jakob.songreporter.config file exists and creating a new one if not
         try {
             loadConfig();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // getting or if needed setting the path to the browserdriver
-        String dropboxPath = configManager.getDropboxPath();
-        File testDropboxFile;
-
-        // getting or if needed setting the path to the users dropbox directory
-        if (dropboxPath == null || dropboxPath.isEmpty()) {
-            dropboxPath = new DropboxSelector().selectDropbox();
-            configManager.setDropboxPath(dropboxPath);
-            configManager.saveConfig();
-        }
-        testDropboxFile = new File(dropboxPath);
-        if (!(testDropboxFile.exists())) {
-            dropboxPath = new DropboxSelector().selectDropbox();
-            configManager.setBrowser(dropboxPath);
-            configManager.saveConfig();
-        }
+        checkConfigValues();
 
         return configManager;
     }
 
-    // function to read the me.jakob.songreporter.config file and create a new configmanager out of this
     private void loadConfig() throws IOException {
-        // testing if the me.jakob.songreporter.config.json File exists and creating a new one if not
         boolean isCreated = true;
         if (!(config.exists())) {
             isCreated = new File(config.getParent()).mkdirs();
@@ -58,12 +35,9 @@ public class ConfigLoader {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             newConfig();
-        // testing if the me.jakob.songreporter.config is empty and creating a new one if it is empty
         } else if (!(new BufferedReader(new InputStreamReader(new FileInputStream(config), StandardCharsets.UTF_8)).ready())) {
             newConfig();
-        // reading the file if everything is ok with it
         } else {
             StringBuilder fromJsonBuilder = new StringBuilder();
             BufferedReader bufferedReader;
@@ -77,29 +51,44 @@ public class ConfigLoader {
                 e.printStackTrace();
             }
 
-            configManager = gson.fromJson(fromJsonBuilder.toString().trim(), ConfigManager.class);
+            try {
+                configManager = gson.fromJson(fromJsonBuilder.toString().trim(), ConfigManager.class);
+            } catch (JsonSyntaxException e) {
+                newConfig();
+            }
         }
 
-        // showing an error window if something went wrong with the me.jakob.songreporter.config file
         if (!isCreated) {
             ErrorGUI jsonErrorGUI = new ErrorGUI();
-            jsonErrorGUI.showNewErrorMessage("Es ist ein Fehler mit der config Datei aufgetreten!");
+            jsonErrorGUI.showNewErrorMessage("Config file couldn't be created.");
         }
     }
 
-    // function to create a new and complete me.jakob.songreporter.config file
     private void newConfig() {
-        // new configmanager to set and save the values
         configManager = new ConfigManager();
-
-        // setting the values
-        String driverPath = new DriverSelector().selectDriver();
-        configManager.setBrowser(driverPath);
-
-        String dropboxPath = new DropboxSelector().selectDropbox();
-        configManager.setDropboxPath(dropboxPath);
-
-        // saving the configuration
         configManager.saveConfig();
+    }
+
+    private void checkConfigValues() {
+        String browser = configManager.getBrowser();
+        if (browser != null && !(browser.equals("Chrome") || browser.equals("Opera") || browser.equals("Firefox"))) {
+            configManager.setBrowser(null);
+        }
+
+        String songsPath = configManager.getSongsDirectory();
+        if (songsPath != null) {
+            File testSongsFile = new File(songsPath);
+            if (!(testSongsFile.exists())) {
+                configManager.setSongsDirectory(null);
+            }
+        }
+
+        String scriptsPath = configManager.getScriptsDirectory();
+        if (scriptsPath != null) {
+            File testScriptsFile = new File(scriptsPath);
+            if (!(testScriptsFile.exists())) {
+                configManager.setScriptsDirectory(null);
+            }
+        }
     }
 }
--- src/main/java/me/jakob/songreporter/config/ConfigManager.java
+++ src/main/java/me/jakob/songreporter/config/ConfigManager.java
@@ -6,18 +6,13 @@ import com.google.gson.GsonBuilder;
 import java.io.*;
 
 public class ConfigManager {
-    // variables for saving the path to the dropbox directory and the browserdriver
     private final transient Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private final transient File config = new File(System.getProperty("user.home") + "/Songreporter/config.json");
-    private transient File script;
-    private transient String tempEMail;
-    private transient String tempPassword;
     private String browser;
-    private String dropboxPath;
+    private String songsDirectory;
+    private String scriptsDirectory;
     private String eMail;
-    private String password;
 
-    // function to save the values into the me.jakob.songreporter.config file
     public void saveConfig() {
         String toJson = this.gson.toJson(this);
         try {
@@ -33,24 +28,16 @@ public class ConfigManager {
         return this.browser;
     }
 
-    public String getDropboxPath() {
-        return this.dropboxPath;
-    }
-
     public void setBrowser(String browser) {
         this.browser = browser;
     }
 
-    public void setDropboxPath(String dropboxPath) {
-        this.dropboxPath = dropboxPath;
+    public String getSongsDirectory() {
+        return this.songsDirectory;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setSongsDirectory(String sonbeamerPath) {
+        this.songsDirectory = sonbeamerPath;
     }
 
     public String getEMail() {
@@ -61,27 +48,11 @@ public class ConfigManager {
         this.eMail = eMail;
     }
 
-    public File getScript() {
-        return script;
+    public String getScriptsDirectory() {
+        return scriptsDirectory;
     }
 
-    public void setScript(File script) {
-        this.script = script;
-    }
-
-    public String getTempEMail() {
-        return tempEMail;
-    }
-
-    public void setTempEMail(String tempEMail) {
-        this.tempEMail = tempEMail;
-    }
-
-    public String getTempPassword() {
-        return tempPassword;
-    }
-
-    public void setTempPassword(String tempPassword) {
-        this.tempPassword = tempPassword;
+    public void setScriptsDirectory(String scriptsDirectory) {
+        this.scriptsDirectory = scriptsDirectory;
     }
 }
