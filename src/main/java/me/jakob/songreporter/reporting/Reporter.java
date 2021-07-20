f6fbfcca697d58532f050a08124bf173ef6fb9e4
Obstmoehre <jakob.gerstmann@outlook.de>
2021-07-21 00:18:52 +0200

    removed test comment

--- src/main/java/me/jakob/songreporter/reporting/Reporter.java
+++ src/main/java/me/jakob/songreporter/reporting/Reporter.java
@@ -33,8 +33,6 @@ public class Reporter {
         this.seleniumService = SeleniumService.getInstance();
     }
 
-    //test
-
     public void report(Config config, File script) {
         try {
             errorWriter = new FileWriter(errorLog, true);
